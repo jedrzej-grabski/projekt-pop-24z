168df251d69a6c521c275123d6a1225ca5fd9550
jedrzej-grabski <grabski.dev@gmail.com>
2025-01-28 20:48:12 +0100

    Cleaned up benchmarking functions

--- src/projekt_pop_24z/benchmark.py
+++ src/projekt_pop_24z/benchmark.py
@@ -1,9 +1,15 @@
-from pathlib import Path
-from typing import Callable, Optional
+from typing import Callable
 from dataclasses import dataclass
-from src.projekt_pop_24z.utils.logger import SwarmLogger
-from projekt_pop_24z.swarm.pso import Coordinates, Bounds, Task, Swarm
-from src.projekt_pop_24z.utils.plotter import PlotDescription, plot_global_best_costs
+
+from tabulate import tabulate
+
+from src.projekt_pop_24z.utils.logger import LogParameters
+from src.projekt_pop_24z.swarm.pso import Task, Swarm, SwarmLogger
+from src.projekt_pop_24z.utils.plotter import PlotDescription, Plotter, PlotType
+
+
+Bounds = list[list[float]]
+Coordinates = list[float]
 
 
 @dataclass
@@ -16,25 +22,29 @@ class AlgorithmParameters:
     initial_inertia: float
     cognitive_constant: float
     social_constant: float
+    dynamic_inertia: bool = False
+    inertia_decay: float = 0
 
 
 @dataclass
 class OptimizationResult:
+    logger: SwarmLogger
+    algorithm_parameters: AlgorithmParameters
     best_position: Coordinates
     best_cost: float
 
 
 def run_single_benchmark(
-    problem_name: str,
     cost_function: Callable[[Coordinates], float],
     parameters: AlgorithmParameters,
-    logging: bool = False,
-    visualize_plot: bool = False,
-    save_plot: bool = False,
-    save_path: Optional[Path] = None,
-) -> OptimizationResult:
+    log_params: LogParameters,
+) -> tuple[OptimizationResult, SwarmLogger]:
 
-    logger = SwarmLogger() if logging else None
+    logger = SwarmLogger(
+        epsilon=log_params.epsilon,
+        name=log_params.name,
+        optimum_position=log_params.optimum_value,
+    )
 
     swarm = Swarm(
         swarm_size=parameters.swarm_size,
@@ -43,6 +53,8 @@ def run_single_benchmark(
         task=parameters.task,
         cost_function=cost_function,
         logger=logger,
+        dynamic_inertia=parameters.dynamic_inertia,
+        inertia_decay=parameters.inertia_decay,
     )
 
     swarm.init_swarm()
@@ -54,18 +66,77 @@ def run_single_benchmark(
         social_constant=parameters.social_constant,
     )
 
-    if visualize_plot or save_plot:
-        if logger is not None:
-            plot_description = PlotDescription(
-                problem_name=problem_name, save_path=save_path
-            )
-            plot_global_best_costs(
-                logger, plot_description, save=save_plot, show=visualize_plot
-            )
-
-        else:
-            raise ValueError("Cannot visualize or save plot without logging")
-
-    return OptimizationResult(
-        best_position=optimization_result, best_cost=cost_function(optimization_result)
+    return (
+        OptimizationResult(
+            logger=logger,
+            algorithm_parameters=parameters,
+            best_position=optimization_result,
+            best_cost=cost_function(optimization_result),
+        ),
+        logger,
     )
+
+
+def run_benchmark_and_plot(
+    cost_function: Callable[[Coordinates], float],
+    parameters: AlgorithmParameters,
+    log_params: LogParameters,
+    plot_description: PlotDescription,
+    plot_types: list[PlotType],
+) -> OptimizationResult:
+    result, logger = run_single_benchmark(cost_function, parameters, log_params)
+    plotter = Plotter(
+        logger=logger,
+        plot_description=plot_description,
+        bounds=parameters.bounds,
+        func=cost_function,
+    )
+
+    for plot_type in plot_types:
+        match plot_type:
+            case PlotType.GLOBAL_BEST_COSTS:
+                plotter.plot_global_best_costs()
+            case PlotType.STARTING_AND_ENDING_POSITIONS:
+                plotter.plot_starting_and_ending_positions_two_dimensional()
+
+    return result
+
+
+def pretty_print_result(result: OptimizationResult) -> None:
+    print("-" * 30)
+    print(f"Benchmark Result for Function: {result.logger.name}")
+    print("-" * 30)
+
+    # Algorithm Parameters as a Table
+    params = result.algorithm_parameters
+    param_table: list[list[object]] = [
+        ["Swarm Size", params.swarm_size],
+        ["Dimensions", params.dimensions],
+        ["Bounds", params.bounds],
+        ["Task", params.task.name],
+        ["Iterations", params.iterations],
+        ["Initial Inertia", params.initial_inertia],
+        ["Cognitive Constant", params.cognitive_constant],
+        ["Social Constant", params.social_constant],
+        ["Dynamic Inertia", "Enabled" if params.dynamic_inertia else "Disabled"],
+    ]
+
+    if params.dynamic_inertia:
+        param_table.append(["Inertia Decay", params.inertia_decay])
+
+    print("\nAlgorithm Parameters:")
+    print(tabulate(param_table, headers=["Parameter", "Value"], tablefmt="grid"))
+
+    # Optimization Results
+    print("\nOptimization Results:")
+    print(f"- Best Position: {[round(pos, 9) for pos in result.best_position]}")
+    print(f"- Best Cost: {result.best_cost:.10f}")
+    if result.logger.epsilon != -1:
+        print(f"- Epsilon: {result.logger.epsilon}")
+        if result.logger.epsilon_reached:
+            print(
+                f"- Iterations until epsilon: {result.logger.iterations_until_epsilon}"
+            )
+        else:
+            print("- Epsilon not reached.")
+    print("\n" + "=|=" * 30 + "\n")
