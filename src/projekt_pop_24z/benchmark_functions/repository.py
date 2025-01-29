5ffd53d6c6e39fb73abd4c0ca8afdf9799dea205
Maksym <bienkowski.maksym@gmail.com>
2025-01-29 20:23:28 +0100

    all funcs

--- src/projekt_pop_24z/benchmark_functions/repository.py
+++ src/projekt_pop_24z/benchmark_functions/repository.py
@@ -2,3 +2,5 @@ from src.projekt_pop_24z.benchmark_functions.rastrigin import Rastrigin
 from src.projekt_pop_24z.benchmark_functions.sphere import Sphere
 from src.projekt_pop_24z.benchmark_functions.rosenbrock import Rosenbrock
 from src.projekt_pop_24z.benchmark_functions.ackley import Ackley
+
+ALL_FUNCS = (Sphere, Rosenbrock, Rastrigin, Ackley)
