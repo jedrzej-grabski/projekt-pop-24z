7345231d00ada469a39c4182f934f6eb84a83f4b
Maksym <bienkowski.maksym@gmail.com>
2025-02-03 17:53:05 +0100

    add cec2017 to all funcs

--- src/projekt_pop_24z/benchmark_functions/repository.py
+++ src/projekt_pop_24z/benchmark_functions/repository.py
@@ -2,6 +2,6 @@ from src.projekt_pop_24z.benchmark_functions.rastrigin import Rastrigin
 from src.projekt_pop_24z.benchmark_functions.sphere import Sphere
 from src.projekt_pop_24z.benchmark_functions.rosenbrock import Rosenbrock
 from src.projekt_pop_24z.benchmark_functions.ackley import Ackley
-from src.projekt_pop_24z.benchmark_functions.cec import CEC2014_F1
+from src.projekt_pop_24z.benchmark_functions.cec import CEC2014_F1, CEC2017_F1
 
-ALL_FUNCS = (Sphere, Rosenbrock, Rastrigin, Ackley, CEC2014_F1)
+ALL_FUNCS = (Sphere, Rosenbrock, Rastrigin, Ackley, CEC2014_F1, CEC2017_F1)
